2be15315d13fc00c425bc73213f69e0d62d09d02
Nick Bobrov <drsfarait@gmail.com>
2024-07-20 20:05:15 +0500

    pyproject write options and exclude &&& widget

--- src/widget.py
+++ src/widget.py
@@ -15,6 +15,14 @@ def mask_account_card(input_string: str) -> str | None:
 
 def get_data(input_string: str) -> str | None:
     """Function convert date"""
-    date = input_string("T")[0]
+    date = input_string.split("T")[0]
     formatted_date = f"{date[-2:]}.{date[5:7]}.{date[:4]}"
     return formatted_date
+
+
+print(mask_account_card('Visa Platinum 8990922113665229'))
+print(mask_account_card('Maestro 1596837868705199'))
+print(mask_account_card('Счет 64686473678894779589'))
+print(mask_account_card('Visa Gold 5999414228426353'))
+print(mask_account_card('Счет 35383033474447895560'))
+print(get_data("2024-03-11T02:26:18.671407"))
